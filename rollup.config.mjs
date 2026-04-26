815c49d0a57f2b6d8e9c28df9486369603577717
Marcus L.W. <marcus.lw@outlook.com>
2026-04-25 23:09:24 -0300

    Configuração do rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,46 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(
+  readFileSync(new URL("./package.json", import.meta.url))
+);
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        skip: ["react", "react-dom"],
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+      postcss({ extensions: [".css"], inject: true }),
+    ],
+    external: ["react", "react-dom", "react/jsx-runtime"]
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  },
+];
